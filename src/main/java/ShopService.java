f43e8b263af89ebb714d86aa739ba10c246cc855
emea94 <emea94@web.de>
2024-02-12 18:29:01 +0100

    add new Exception and Implemented it into method addOrder

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -8,13 +8,15 @@ public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds)  //Bei Checked-Exception: throws ProductNotFoundException
+    {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
             Optional<Product> productToOrder = productRepo.getProductById(productId);
             if (productToOrder.isEmpty()) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
+                throw new ProductNotFoundException("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+                // anstatt: System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+                //                return null;
             }
             products.add(productToOrder.get());
         }
